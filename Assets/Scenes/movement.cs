5559a821f61e018f2182a15444d7780bebce46e7
tuthth <tuannase160981@fpt.edu.vn>
2023-10-29 14:09:06 +0700

    test enemy

--- Assets/Scenes/movement.cs
+++ Assets/Scenes/movement.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class movement : MonoBehaviour
 {
     private float horizontal;
-    private float speed = 8f;
+    [SerializeField] private float speed = 8f;
     private float jumpingPower = 16f;
     private bool isFacingRight = true;
 
